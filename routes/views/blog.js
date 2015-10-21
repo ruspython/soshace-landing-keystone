4cf7b534d8db6b3786264eb672d77af8c0160d42
Vladislav <ruspython@gmail.com>
2015-10-21 14:11:25 +0300

    Blog page

--- routes/views/blog.js
+++ routes/views/blog.js
@@ -7,71 +7,28 @@ exports = module.exports = function(req, res) {
 	var locals = res.locals;
 	
 	// Init locals
-	locals.section = 'blog';
+	locals.postSection = true;
 	locals.filters = {
 		category: req.params.category
 	};
 	locals.data = {
-		posts: [],
-		categories: []
+		posts: []
 	};
-	
-	// Load all categories
-	view.on('init', function(next) {
-		
-		keystone.list('PostCategory').model.find().sort('name').exec(function(err, results) {
-			
-			if (err || !results.length) {
-				return next(err);
-			}
-			
-			locals.data.categories = results;
-			
-			// Load the counts for each category
-			async.each(locals.data.categories, function(category, next) {
-				
-				keystone.list('Post').model.count().where('categories').in([category.id]).exec(function(err, count) {
-					category.postCount = count;
-					next(err);
-				});
-				
-			}, function(err) {
-				next(err);
-			});
-			
-		});
-		
-	});
-	
-	// Load the current category filter
-	view.on('init', function(next) {
-		
-		if (req.params.category) {
-			keystone.list('PostCategory').model.findOne({ key: locals.filters.category }).exec(function(err, result) {
-				locals.data.category = result;
-				next(err);
-			});
-		} else {
-			next();
-		}
-		
-	});
 	
 	// Load the posts
 	view.on('init', function(next) {
 		
-		var q = keystone.list('Post').paginate({
-				page: req.query.page || 1,
-				perPage: 10,
-				maxPages: 10
-			})
+		var q = keystone.list('Post')
+			//.paginate({
+			//	page: req.query.page || 1,
+			//	perPage: 10,
+			//	maxPages: 10
+			//})
+			//TODO: infinite scroll would be better here
+			.model.find()
 			.where('state', 'published')
 			.sort('-publishedDate')
-			.populate('author categories');
-		
-		if (locals.data.category) {
-			q.where('categories').in([locals.data.category]);
-		}
+			;
 		
 		q.exec(function(err, results) {
 			locals.data.posts = results;
@@ -79,7 +36,7 @@ exports = module.exports = function(req, res) {
 		});
 		
 	});
-	
+
 	// Render the view
 	view.render('blog');
 	
