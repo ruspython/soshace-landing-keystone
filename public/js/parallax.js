f823f91683bbdc25c8fcd6b09ccfe9ef990131c3
vlad <ruspython@gmail.com>
2016-04-15 12:41:47 +0300

    logo z-index fix

--- public/js/parallax.js
+++ public/js/parallax.js
@@ -3,13 +3,17 @@
 (function () {
     'use strict';
     var video = document.querySelector('.intro__video'),
-        header = document.querySelector('.main-header');
+        header = document.querySelector('.main-header'),
+        logo = document.querySelector('.intro__inner')
+        ;
     var HEADER_HEIGHT = parseInt(window.getComputedStyle(header).height);
 
     window.onscroll = function () {
         if ((scrollY + HEADER_HEIGHT) < innerHeight) {
-            var change = (scrollY / innerHeight);
             video.style.transform = 'translate3d(-960px, ' + (-540 + scrollY / 4) + 'px, 0)';
+            logo.style.display = '';
+        } else {
+            logo.style.display = 'none';
         }
     }
 
