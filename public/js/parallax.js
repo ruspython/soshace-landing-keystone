062888a5d94b6be7d913cc6e16ab65ea9645e513
vlad <ruspython@gmail.com>
2016-04-21 13:56:13 +0300

    quick logo fix

--- public/js/parallax.js
+++ public/js/parallax.js
@@ -5,17 +5,18 @@
     var $video = $('.intro__video'),
         $header = $('.main-header'),
         $logo = $('.intro__inner'),
+        $features = $('.features'),
         scrollY = $(window).scrollTop()
         ;
     var HEADER_HEIGHT = parseInt($header.height());
 
-    if ((scrollY + HEADER_HEIGHT) < innerHeight) {
+    if ($logo.offset().top < $features.offset().top) {
         $logo.show();
     }
 
     $(window).on('scroll', function () {
         scrollY = $(window).scrollTop();
-        if (scrollY < innerHeight/2) {
+        if ($logo.offset().top < $features.offset().top) {
             $video.css('transform', 'translate3d(-960px, ' + (-540 + scrollY / 4) + 'px, 0)');
             $logo.show();
         } else {
