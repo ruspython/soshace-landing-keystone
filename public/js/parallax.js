42e9fdeafdf4eee68c8a3b9961379120280e2a9b
vlad <ruspython@gmail.com>
2016-04-21 13:21:35 +0300

    bug fixing

--- public/js/parallax.js
+++ public/js/parallax.js
@@ -1,24 +1,25 @@
 // Making logo
 
-(function () {
+(function ($) {
     'use strict';
-    var video = document.querySelector('.intro__video'),
-        header = document.querySelector('.main-header'),
-        logo = document.querySelector('.intro__inner')
+    var $video = $('.intro__video'),
+        $header = $('.main-header'),
+        $logo = $('.intro__inner'),
+        scrollY = $(window).scrollTop()
         ;
-    var HEADER_HEIGHT = parseInt(window.getComputedStyle(header).height);
+    var HEADER_HEIGHT = parseInt($header.height());
 
     if ((scrollY + HEADER_HEIGHT) < innerHeight) {
-        logo.style.display = '';
+        $logo.show();
     }
 
-    window.onscroll = function () {
-        if ((scrollY + HEADER_HEIGHT) < innerHeight) {
-            video.style.transform = 'translate3d(-960px, ' + (-540 + scrollY / 4) + 'px, 0)';
-            logo.style.display = '';
+    $(window).on('scroll', function () {
+        scrollY = $(window).scrollTop();
+        if (scrollY < innerHeight/2) {
+            $video.css('transform', 'translate3d(-960px, ' + (-540 + scrollY / 4) + 'px, 0)');
+            $logo.show();
         } else {
-            logo.style.display = 'none';
+            $logo.hide();
         }
-    }
-
-})();
+    })
+})(jQuery);
