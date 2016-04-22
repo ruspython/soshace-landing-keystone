3069e3da6a0a96e44e1ecec77463b45a7d9e847b
vlad <ruspython@gmail.com>
2016-04-22 17:56:25 +0300

    logo position fix

--- public/js/parallax.js
+++ public/js/parallax.js
@@ -5,8 +5,10 @@
     var $video = $('.intro__video'),
         $header = $('.main-header'),
         $logo = $('.intro__inner'),
+        $intro = $('.intro'),
         $features = $('.features'),
-        scrollY = $(window).scrollTop()
+        scrollY = $(window).scrollTop(),
+        scrollMax = $(window).height() - $logo.offset().top
         ;
     var HEADER_HEIGHT = parseInt($header.height());
 
@@ -16,7 +18,8 @@
 
     $(window).on('scroll', function () {
         scrollY = $(window).scrollTop();
-        if ($logo.offset().top < $features.offset().top) {
+
+        if (scrollY < scrollMax) {
             $video.css('transform', 'translate3d(-960px, ' + (-540 + scrollY / 4) + 'px, 0)');
             $logo.show();
         } else {
