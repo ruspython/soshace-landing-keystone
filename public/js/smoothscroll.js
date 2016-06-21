eb706e06147cbf58f788d38370ad38f325b48ec2
Andrey Medvedev <andr.medv.spb@gmail.com>
2016-06-21 11:05:27 +0300

    Replace smooth scrolling by jump to anchor for mobile devices
    Because on mobile devices smooth scrolling not look great yet. It's
    freezing for long jumps to anchor links, and looks not so smooth.
    
    I try to use default handler for mobile devices, but on mobile chrome
    and all firefox links do not work.
    So i added window.scrollTo for mobile width and on all mobile devices
    links works.
    
    iss #2013

--- public/js/smoothscroll.js
+++ public/js/smoothscroll.js
@@ -1,35 +1,43 @@
 /**
-* anchor-scroll
-* @author Benjamin De Cock
-* @repo https://github.com/bendc/anchor-scroll
+* Smooth Scrolling
+* @author CHRIS COYIER
+* example from https://css-tricks.com/snippets/jquery/smooth-scrolling/
 */
-
 'use strict';
 (function() {
+
+  var SMALL_DISPLAYS_WIDTH_BORDER = 699;
+
   document.addEventListener('DOMContentLoaded', function() {
 
     var links = document.querySelectorAll('.scroll');
-    var i = links.length;
 
-    while (i--)
+    for (var i = 0; i < links.length; i++) {
+      links.item(i).addEventListener('click', smoothScrolling);
+    }
 
-      links.item(i).addEventListener('tap', function(e) {
+  });
 
-        if (window.innerWidth > 699) {
-          e.preventDefault();
-          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            if (target.length) {
-              $('html, body').animate({
-                scrollTop: target.offset().top
-              }, 1500);
-              return false;
-            }
-          }
+  function smoothScrolling(event) {
+
+    event.preventDefault();
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+
+        if (window.innerWidth < SMALL_DISPLAYS_WIDTH_BORDER) {
+          // just jump
+          window.scrollTo(0, target.offset().top);
+        } else {
+          // smooth scroll
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1500);
         }
 
-      });
-
-    });
+        return false;
+      }
+    }
+  }
 })();
