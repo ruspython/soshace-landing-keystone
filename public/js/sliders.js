ed9247a8dcb28a7ec3545e2f956142d7d4798658
Andrey Medvedev <andr.medv.spb@gmail.com>
2016-06-23 17:23:35 +0300

    Replace jquery ready function by DOMContentLoaded event for sliders

--- public/js/sliders.js
+++ public/js/sliders.js
@@ -1,45 +1,48 @@
-'use strict';
+;(function() {
+  'use strict';
 
-$(document).ready(function() {
+  document.addEventListener("DOMContentLoaded", initSliders);
 
-  var portfolioSlider = new Swiper('.portfolio__slider', {
-    loop: true,
-    prevButton: $('.portfolio__arrow-wrap--left'),
-    nextButton: $('.portfolio__arrow-wrap--right'),
-    slidesPerView: 2,
-    spaceBetween: "5%",
-    runCallbacksOnInit: true,
-    breakpoints: {
-      // when window width is <= 959px
-      959: {
-        slidesPerView: 1,
-        spaceBetween: 0
+  function initSliders() {
+    var portfolioSlider = new Swiper('.portfolio__slider', {
+      loop: true,
+      prevButton: $('.portfolio__arrow-wrap--left'),
+      nextButton: $('.portfolio__arrow-wrap--right'),
+      slidesPerView: 2,
+      spaceBetween: "5%",
+      runCallbacksOnInit: true,
+      breakpoints: {
+        // when window width is <= 959px
+        959: {
+          slidesPerView: 1,
+          spaceBetween: 0
+        }
       }
-    }
-  });
+    });
 
-  portfolioSlider.on('tap', portfolioModal.openModal);
+    portfolioSlider.on('tap', portfolioModal.openModal);
 
-  var teamSlider = new Swiper('.team__members-wrap', {
-    loop: true,
-    prevButton: $('.team__arrow-wrap--left'),
-    nextButton: $('.team__arrow-wrap--right'),
-    slidesPerView: 3,
-    spaceBetween: 20,
-    breakpoints: {
-      // when window width is <= 1199px
-      1199: {
-        slidesPerView: 2,
-        spaceBetween: 10,
-      },
-      699: {
-        slidesPerView: 1,
-        spaceBetween: 0,
+    var teamSlider = new Swiper('.team__members-wrap', {
+      loop: true,
+      prevButton: $('.team__arrow-wrap--left'),
+      nextButton: $('.team__arrow-wrap--right'),
+      slidesPerView: 3,
+      spaceBetween: 20,
+      breakpoints: {
+        // when window width is <= 1199px
+        1199: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        699: {
+          slidesPerView: 1,
+          spaceBetween: 0,
+        }
       }
-    }
-  });
+    });
 
-  teamSlider.on('tap', teamModal.openModal);
+    teamSlider.on('tap', teamModal.openModal);
+  }
 
-});
+}());
 
